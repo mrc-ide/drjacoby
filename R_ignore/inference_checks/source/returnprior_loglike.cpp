f080ed5d354c9b3565d8b13a816221d79d96beef
Peter Winskill <p.winskill@imperial.ac.uk>
2021-02-26 15:27:26 +0000

    Update inference checks to work with new cpp specification

--- R_ignore/inference_checks/source/returnprior_loglike.cpp
+++
@@ -1,9 +0,0 @@
-#include <Rcpp.h>
-using namespace Rcpp;
-
-// [[Rcpp::export]]
-// drjacoby_function_start
-SEXP loglike(Rcpp::NumericVector params, Rcpp::List data, Rcpp::List misc) {
-  return Rcpp::wrap(0.0);
-}
-// drjacoby_function_end
