253dbd7a6eb09df3f952f0be860534204e7a154b
Bob Verity <bobverity1@gmail.com>
2021-09-24 08:40:49 +0100

    added test on -Inf likelihoods and priors. Checks that hottest rung always explores prior, even when likelihood is zero

--- tests/testthat/test-Inf_checks_work.R
+++ tests/testthat/test-Inf_checks_work.R
@@ -1,3 +1,4 @@
+#------------------------------------------------
 test_that("Checks on likelihood and prior errors work", {
   
   # define true parameter values
@@ -92,7 +93,7 @@ test_that("Checks on likelihood and prior errors work", {
   
 })
 
-
+#------------------------------------------------
 test_that("Negative Inf initialisation checks", {
   # define true parameter values
   mu_true <- 3
@@ -148,5 +149,91 @@ test_that("Negative Inf initialisation checks", {
                         burnin = 1e3,
                         samples = 1e3,
                         chains = 1))
+  
+})
+
+#------------------------------------------------
+test_that("-Inf tolerated in likelihood and prior", {
+  
+  # define parameter mu with [0,1] range
+  df_params <- define_params(name = "mu", min = 0, max = 1, init = 0.1)
+  
+  # likelihood is -Inf for half the domain
+  r_loglike <- function(params, data, misc) {
+    mu <- params["mu"]
+    if (mu > 0.5) {
+      return(-Inf)
+    } else {
+      return(2)
+    }
+  }
+  
+  # prior is finite everywhere
+  r_logprior <- function(params, misc) {
+    0
+  }
+  
+  # run mcmc with two rungs at 0 and 1
+  mcmc <- run_mcmc(data = list(x = 1),
+                   df_params = df_params,
+                   loglike = r_loglike,
+                   logprior = r_logprior,
+                   burnin = 1e3,
+                   samples = 1e3,
+                   chains = 1,
+                   beta_manual = c(0, 1))
+  
+  # get maximum value of mu for each rung. Should be >0.5 for rung1 and <0.5 for
+  # rung2
+  mu_max_df <- mcmc$output %>%
+    dplyr::group_by(rung) %>%
+    dplyr::summarise(mu = max(mu))
+  
+  expect_equal((mu_max_df$mu > 0.5), c(TRUE, FALSE))
+  
+  # rung1 should contain -Inf loglike values whenever mu > 0.5, and finite values
+  # otherwise
+  rung1 <- mcmc$output %>%
+    dplyr::filter(rung == 1)
+  
+  expect_true(all(rung1$loglikelihood[rung1$mu > 0.5] == -Inf))
+  expect_true(all(is.finite(rung1$loglikelihood[rung1$mu < 0.5])))
+  
+  # expect no errors when plotting loglikelihoods
+  expect_error(plot_rung_loglike(mcmc, y_axis_type = 1), NA)
+  expect_error(plot_rung_loglike(mcmc, y_axis_type = 2), NA)
+  expect_error(plot_rung_loglike(mcmc, y_axis_type = 3), NA)
+  
+  
+  ################################
+  
+  
+  # same as above, but with -Inf implemented in prior not likelihood
+  r_loglike <- function(params, data, misc) {
+    0
+  }
+  
+  # prior is finite everywhere
+  r_logprior <- function(params, misc) {
+    mu <- params["mu"]
+    if (mu > 0.5) {
+      return(-Inf)
+    } else {
+      return(2)
+    }
+  }
+  
+  # run mcmc with two rungs at 0 and 1
+  mcmc <- run_mcmc(data = list(x = 1),
+                   df_params = df_params,
+                   loglike = r_loglike,
+                   logprior = r_logprior,
+                   burnin = 1e3,
+                   samples = 1e3,
+                   chains = 1,
+                   beta_manual = c(0, 1))
+  
+  # now all mu values should be in the range [0,0.5], irrespective of rung
+  expect_true(all(mcmc$output$mu < 0.5))
   
 })
